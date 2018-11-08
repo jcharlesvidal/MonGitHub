dff6a1d12f67115a27907c2c0df2956255bd908e
1802448 <1802448@POR7400329.exchange.ad.afpanet>
2018-11-08 11:27:56 +0100

    modif main

--- src/main/Main.java
+++ src/main/Main.java
@@ -3,7 +3,8 @@ package main;
 public class Main {
 
 	public static void main(String[] args) {
-		System.out.println("Hello toi aussi, oui toi !");
+		Stuff stuff = new Stuff();
+		System.out.println(stuff.getHello());
 	}
 
 }
